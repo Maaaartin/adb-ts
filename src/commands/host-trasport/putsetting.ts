f638158d9bc4eb98097eb426b8aeb345d6d98026
Maaaartin <svobma96@gmail.com>
2021-03-14 18:30:31 +0100

    Set property fix

--- src/commands/host-trasport/putsetting.ts
+++ src/commands/host-trasport/putsetting.ts
@@ -1,8 +1,17 @@
-import { SettingsMode, SimpleType } from "../..";
-import TransportCommand from "../tranport";
+import { SettingsMode, SimpleType } from '../..';
+import TransportCommand from '../tranport';
 
 export default class PutSetting extends TransportCommand {
-    execute(serial: string, mode: SettingsMode, name: string, value: SimpleType) {
-        return super.execute(serial, 'shell:settings put', mode, name, this.escape(value)).return();
-    }
+  execute(serial: string, mode: SettingsMode, name: string, value: SimpleType) {
+    return super
+      .execute(serial, 'shell:settings put', mode, name, this.escape(value))
+      .then(() => {
+        return this.parser.readAll().then((value) => {
+          const valueStr = value.toString();
+          if (/failed/.test(valueStr)) {
+            throw new Error(valueStr);
+          }
+        });
+      });
+  }
 }
