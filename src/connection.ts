85a9b39bf9257709d547d1e1014d700df67a1ce4
Martin Svoboda <svobma96@gmail.com>
2022-07-07 16:40:01 +0200

    Fix eslint errors in Connection

--- src/connection.ts
+++ src/connection.ts
@@ -16,39 +16,39 @@ export default class Connection extends Socket {
         );
     }
 
-    get readBytes() {
+    get readBytes(): typeof Parser.prototype.readBytes {
         return this.parser.readBytes;
     }
 
-    get endParser() {
+    get endParser(): typeof Parser.prototype.end {
         return this.parser.end;
     }
 
-    get readAscii() {
+    get readAscii(): typeof Parser.prototype.readAscii {
         return this.parser.readAscii;
     }
 
-    get readValue() {
+    get readValue(): typeof Parser.prototype.readValue {
         return this.parser.readValue;
     }
 
-    get readError() {
+    get readError(): typeof Parser.prototype.readError {
         return this.parser.readError;
     }
 
-    get unexpected() {
+    get unexpected(): typeof Parser.prototype.unexpected {
         return this.parser.unexpected;
     }
 
-    get readByteFlow() {
+    get readByteFlow(): typeof Parser.prototype.readByteFlow {
         return this.parser.readByteFlow;
     }
 
-    get searchLine() {
+    get searchLine(): typeof Parser.prototype.searchLine {
         return this.parser.searchLine;
     }
 
-    get readAll() {
+    get readAll(): typeof Parser.prototype.readAll {
         return this.parser.readAll;
     }
 }
