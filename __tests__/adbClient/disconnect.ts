5a73cd45ab7a41a611b737ffc4e287e76f5d2924
Martin Svoboda <svobma96@gmail.com>
2022-07-10 13:24:58 +0200

    Use new adb mock in disconnect tests

--- __tests__/adbClient/disconnect.ts
+++ __tests__/adbClient/disconnect.ts
@@ -1,56 +1,68 @@
-import { mockServer } from '../../mockery/mockAdbServer';
+import { AdbMock, mockServer } from '../../mockery/mockAdbServer';
 import AdbClient from '../../lib/client';
 import { promisify } from 'util';
 
 describe('Disconnect', () => {
     it('Disconnect from default port', async () => {
-        const { port, done } = await mockServer({
-            expValue: 'host:disconnect:127.0.0.1:5555',
-            res: 'disconnected'
-        });
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:disconnect:127.0.0.1:5555',
+                res: 'disconnected'
+            }
+        ]);
         try {
+            const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             const result = await adb.disconnect('127.0.0.1');
             expect(result).toBe('127.0.0.1:5555');
         } finally {
-            await done();
+            await adbMock.end();
         }
     });
 
     it('Disconnect from passed port', async () => {
-        const { port, done } = await mockServer({
-            expValue: 'host:disconnect:127.0.0.1:4444',
-            res: 'disconnected'
-        });
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:disconnect:127.0.0.1:4444',
+                res: 'disconnected'
+            }
+        ]);
         try {
+            const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             const result = await adb.disconnect('127.0.0.1', 4444);
             expect(result).toBe('127.0.0.1:4444');
         } finally {
-            await done();
+            await adbMock.end();
         }
     });
 
     it('Disconnect - host containing port', async () => {
-        const { port, done } = await mockServer({
-            expValue: 'host:disconnect:127.0.0.1:4444',
-            res: 'disconnected'
-        });
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:disconnect:127.0.0.1:4444',
+                res: 'disconnected'
+            }
+        ]);
         try {
+            const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             const result = await adb.disconnect('127.0.0.1:4444');
             expect(result).toBe('127.0.0.1:4444');
         } finally {
-            await done();
+            await adbMock.end();
         }
     });
 
     it('Invalid reply', async () => {
-        const { port, done } = await mockServer({
-            expValue: 'host:disconnect:127.0.0.1:4444',
-            res: 'invalid'
-        });
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:disconnect:127.0.0.1:4444',
+                res: 'invalid'
+            }
+        ]);
         try {
+            const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             try {
                 await adb.disconnect('127.0.0.1', 4444);
@@ -58,23 +70,26 @@ describe('Disconnect', () => {
                 expect(e.message).toBe('invalid');
             }
         } finally {
-            await done();
+            await adbMock.end();
         }
     });
 
     it('Connect callback overload', async () => {
-        const { port, done } = await mockServer({
-            expValue: 'host:disconnect:127.0.0.1:4444',
-            res: 'disconnected'
-        });
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:disconnect:127.0.0.1:4444',
+                res: 'disconnected'
+            }
+        ]);
         try {
+            const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             const result = await promisify<string>((cb) => {
                 adb.disconnect('127.0.0.1', 4444, cb);
             })();
             expect(result).toBe('127.0.0.1:4444');
         } finally {
-            await done();
+            await adbMock.end();
         }
     });
 });
