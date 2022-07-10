06cb5672bfce5b9768e40c33d1b13adb942e64f9
Martin Svoboda <svobma96@gmail.com>
2022-07-10 20:33:39 +0200

    Fix get ip address

--- __tests__/adbClient/ipAddress.ts
+++ __tests__/adbClient/ipAddress.ts
@@ -2,7 +2,7 @@ import AdbMock from '../../mockery/mockAdbServer';
 import AdbClient from '../../lib/client';
 
 describe('IP address', () => {
-    it('OKAY', async () => {
+    it('Single address', async () => {
         const adbMock = new AdbMock([
             { cmd: 'host:transport:serial', res: null, rawRes: true },
             {
@@ -17,6 +17,46 @@ describe('IP address', () => {
             const adb = new AdbClient({ noAutoStart: true, port });
             const result = await adb.getIpAddress('serial');
             expect(result).toBe('127.0.0.1');
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Multiple addresses', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: "shell:ip route | awk '{ print $9 }'",
+                res: '127.0.0.1\n127.0.0.2',
+                rawRes: true
+            }
+        ]);
+
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.getIpAddress('serial');
+            expect(result).toEqual(['127.0.0.1', '127.0.0.2']);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('No address', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: "shell:ip route | awk '{ print $9 }'",
+                res: null,
+                rawRes: true
+            }
+        ]);
+
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.getIpAddress('serial');
+            expect(result).toBe(null);
         } finally {
             await adbMock.end();
         }
--- src/commands/host-trasport/ipaddress.ts
+++ src/commands/host-trasport/ipaddress.ts
@@ -1,17 +1,24 @@
-import ipRegex from 'ip-regex';
 import TransportCommand from '../tranport';
 
-export default class GetIpAddressCommand extends TransportCommand<string> {
-    protected postExecute(): Promise<string> {
+export default class GetIpAddressCommand extends TransportCommand<
+    string | string[] | null
+> {
+    protected postExecute(): Promise<string | string[] | null> {
         return this.parser.readAll().then((value) => {
-            const valueStr = value.toString().trim();
-            return ipRegex().test(valueStr) ? valueStr : '';
+            const addresses = value.toString().trim().split('\n');
+            if (!addresses[0]) {
+                return null;
+            }
+            if (addresses.length === 1) {
+                return addresses[0].trim();
+            }
+            return addresses.map((a) => a.trim());
         });
     }
     get Cmd(): string {
         return "shell:ip route | awk '{ print $9 }'";
     }
-    execute(serial: string): Promise<string> {
+    execute(serial: string): Promise<string | string[] | null> {
         return this.preExecute(serial);
     }
 }
