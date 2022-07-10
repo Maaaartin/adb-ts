3991a4c40031e77686c772b7c78a60b7f89410fd
Martin Svoboda <svobma96@gmail.com>
2022-07-10 15:57:40 +0200

    Use new adb mock in get device path tests

--- __tests__/adbClient/getDevicePath.ts
+++ __tests__/adbClient/getDevicePath.ts
@@ -1,18 +1,18 @@
-import { mockServer } from '../../mockery/mockAdbServer';
+import { AdbMock } from '../../mockery/mockAdbServer';
 import AdbClient from '../../lib/client';
 
 describe('Get device path', () => {
     it('OKAY', async () => {
-        const { port, done } = await mockServer({
-            expValue: 'host-serial:serial:get-devpath',
-            res: 'usb:336592896X'
-        });
+        const adbMock = new AdbMock([
+            { cmd: 'host-serial:serial:get-devpath', res: 'usb:336592896X' }
+        ]);
         try {
+            const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             const result = await adb.getDevicePath('serial');
             expect(result).toBe('usb:336592896X');
         } finally {
-            await done();
+            await adbMock.end();
         }
     });
 });
