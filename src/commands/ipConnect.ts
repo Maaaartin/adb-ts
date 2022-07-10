1b03a816691db47e6c351f0d22e85b88616d39a3
Martin Svoboda <svobma96@gmail.com>
2022-07-10 21:02:55 +0200

    Fix ip connect

--- src/commands/ipConnect.ts
+++ src/commands/ipConnect.ts
@@ -4,16 +4,22 @@ import Command from '../command';
 export default abstract class IpConnect extends Command implements ICmd {
     protected abstract validator(): RegExp;
     abstract Cmd: string;
+    keepAlive = true;
     execute(host: string, port: number | string): Promise<string> {
         return this.initExecute(`${this.Cmd}:${host}:${port}`).then(
             this.handleReply(() => {
-                return this.parser.readValue().then((value) => {
-                    const valueStr = value.toString().trim();
-                    if (this.validator().test(valueStr)) {
-                        return `${host}:${port}`;
-                    }
-                    throw new Error(valueStr);
-                });
+                return this.parser
+                    .readValue()
+                    .then((value) => {
+                        const valueStr = value.toString().trim();
+                        if (this.validator().test(valueStr)) {
+                            return `${host}:${port}`;
+                        }
+                        throw new Error(valueStr);
+                    })
+                    .finally(() => {
+                        return this.end();
+                    });
             })
         );
     }
