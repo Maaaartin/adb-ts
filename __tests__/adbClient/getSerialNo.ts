846c7fb78f53c7b4f03ab195675b7953f1ac2b0b
Martin Svoboda <svobma96@gmail.com>
2022-07-12 19:22:28 +0200

    Get serial no fail test

--- __tests__/adbClient/getSerialNo.ts
+++ __tests__/adbClient/getSerialNo.ts
@@ -16,4 +16,22 @@ describe('Get serial no', () => {
             await adbMock.end();
         }
     });
+
+    it('FAIL first response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'shell:getprop ro.serialno', res: 'test', rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.getSerialNo('fail');
+            } catch (e) {
+                expect(e.message).toBe('Failure');
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
 });
