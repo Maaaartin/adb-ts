f5fb3a4434b75630a391efca35a1d08c23c3c3ee
Martin Svoboda <svobma96@gmail.com>
2022-07-22 21:14:21 +0200

    List packages tests

---
+++ __tests__/adbClient/listPackages.ts
@@ -0,0 +1,140 @@
+import AdbMock from '../../mockery/mockAdbServer';
+import AdbClient from '../../lib/client';
+import { DataMap } from '../../lib';
+
+describe('List packages', () => {
+    it('OKAY', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'shell:pm list packages 2>/dev/null',
+                res: `package:one
+package:two.three
+package:four`,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.listPackages('serial');
+            expect(result).toEqual(['one', 'two.three', 'four']);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL first response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'fail', res: null, rawRes: true },
+            {
+                cmd: 'shell:pm list packages 2>/dev/null',
+                res: `package:one
+package:two.three
+package:four`,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.listPackages('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL second response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'fail',
+                res: `package:one
+package:two.three
+package:four`,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.listPackages('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected first response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            },
+            {
+                cmd: 'shell:pm list packages 2>/dev/null',
+                res: `package:one
+package:two.three
+package:four`,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.listPackages('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected second response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: 'shell:pm list packages 2>/dev/null',
+                res: `package:one
+package:two.three
+package:four`,
+                rawRes: true,
+                unexpected: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.listPackages('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
