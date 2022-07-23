d1499aa81aabd0bdaf97a99649ba7ceec554f559
Martin Svoboda <svobma96@gmail.com>
2022-07-23 17:28:45 +0200

    Refactor forward command, more forward tests

--- __tests__/adbClient/forward.ts
+++ __tests__/adbClient/forward.ts
@@ -1,5 +1,6 @@
 import AdbMock from '../../mockery/mockAdbServer';
 import AdbClient from '../../lib/client';
+import { UnexpectedDataError } from '../../lib';
 
 describe('Forward tests', () => {
     it('OKAY', async () => {
@@ -19,6 +20,62 @@ describe('Forward tests', () => {
                 'localabstract:chrome_devtools_remote'
             );
             expect(result).toBe(void 0);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'fail',
+                res: 'OKAY',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.forward(
+                    'serial',
+                    'tcp:9222',
+                    'localabstract:chrome_devtools_remote'
+                );
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host-serial:serial:forward:tcp:9222;localabstract:chrome_devtools_remote',
+                res: 'OKAY',
+                rawRes: true,
+                unexpected: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.forward(
+                    'serial',
+                    'tcp:9222',
+                    'localabstract:chrome_devtools_remote'
+                );
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toBeInstanceOf(UnexpectedDataError);
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
         } finally {
             await adbMock.end();
         }
--- src/commands/host-serial/forward.ts
+++ src/commands/host-serial/forward.ts
@@ -5,31 +5,12 @@ export default class ForwardCommand extends Command<void> {
     execute(serial: string, local: string, remote: string): Promise<void> {
         return this.initExecute(
             `host-serial:${serial}:forward:${local};${remote}`
-        ).then((reply) => {
-            switch (reply) {
-                case Reply.OKAY:
-                    return this.parser.readAscii(4).then((reply) => {
-                        switch (reply) {
-                            case Reply.OKAY:
-                                return;
-                            case Reply.FAIL:
-                                return this.parser.readError().then((e) => {
-                                    throw e;
-                                });
-                            default:
-                                throw this.parser.unexpected(
-                                    reply,
-                                    'OKAY or FAIL'
-                                );
-                        }
-                    });
-                case Reply.FAIL:
-                    return this.parser.readError().then((e) => {
-                        throw e;
-                    });
-                default:
-                    throw this.parser.unexpected(reply, 'OKAY or FAIL');
-            }
-        });
+        ).then(
+            this.handleReply(() =>
+                (this.parser.readAscii(4) as Promise<Reply>).then(
+                    this.handleReply(void 0)
+                )
+            )
+        );
     }
 }
