ca28c8d8af8e6567dda68d00f118871945579757
Martin Svoboda <svobma96@gmail.com>
2022-07-24 19:34:34 +0200

    Reboot tests

--- __tests__/adbClient/reboot.ts
+++ __tests__/adbClient/reboot.ts
@@ -1,23 +1,17 @@
 import AdbMock from '../../mockery/mockAdbServer';
 import AdbClient from '../../lib/client';
 
-describe('List packages', () => {
+describe('Reboot', () => {
     it('OKAY', async () => {
         const adbMock = new AdbMock([
             { cmd: 'host:transport:serial', res: null, rawRes: true },
-            {
-                cmd: 'shell:pm list packages 2>/dev/null',
-                res: `package:one
-package:two.three
-package:four`,
-                rawRes: true
-            }
+            { cmd: 'reboot:', res: null, rawRes: true }
         ]);
         try {
             const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
-            const result = await adb.listPackages('serial');
-            expect(result).toEqual(['one', 'two.three', 'four']);
+            const result = await adb.reboot('serial');
+            expect(result).toBe(void 0);
         } finally {
             await adbMock.end();
         }
@@ -26,19 +20,13 @@ package:four`,
     it('FAIL first response', async () => {
         const adbMock = new AdbMock([
             { cmd: 'fail', res: null, rawRes: true },
-            {
-                cmd: 'shell:pm list packages 2>/dev/null',
-                res: `package:one
-package:two.three
-package:four`,
-                rawRes: true
-            }
+            { cmd: 'reboot:', res: null, rawRes: true }
         ]);
         try {
             const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             try {
-                await adb.listPackages('serial');
+                await adb.reboot('serial');
                 fail('Expected Failure');
             } catch (e) {
                 expect(e).toEqual(new Error('Failure'));
@@ -51,19 +39,13 @@ package:four`,
     it('FAIL second response', async () => {
         const adbMock = new AdbMock([
             { cmd: 'host:transport:serial', res: null, rawRes: true },
-            {
-                cmd: 'fail',
-                res: `package:one
-package:two.three
-package:four`,
-                rawRes: true
-            }
+            { cmd: 'fail', res: null, rawRes: true }
         ]);
         try {
             const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             try {
-                await adb.listPackages('serial');
+                await adb.reboot('serial');
                 fail('Expected Failure');
             } catch (e) {
                 expect(e).toEqual(new Error('Failure'));
@@ -81,19 +63,13 @@ package:four`,
                 rawRes: true,
                 unexpected: true
             },
-            {
-                cmd: 'shell:pm list packages 2>/dev/null',
-                res: `package:one
-package:two.three
-package:four`,
-                rawRes: true
-            }
+            { cmd: 'reboot:', res: null, rawRes: true }
         ]);
         try {
             const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             try {
-                await adb.listPackages('serial');
+                await adb.reboot('serial');
                 fail('Expected Failure');
             } catch (e) {
                 expect(e).toEqual(
@@ -112,20 +88,13 @@ package:four`,
                 res: null,
                 rawRes: true
             },
-            {
-                cmd: 'shell:pm list packages 2>/dev/null',
-                res: `package:one
-package:two.three
-package:four`,
-                rawRes: true,
-                unexpected: true
-            }
+            { cmd: 'reboot:', res: null, rawRes: true, unexpected: true }
         ]);
         try {
             const port = await adbMock.start();
             const adb = new AdbClient({ noAutoStart: true, port });
             try {
-                await adb.listPackages('serial');
+                await adb.reboot('serial');
                 fail('Expected Failure');
             } catch (e) {
                 expect(e).toEqual(
--- src/commands/host-trasport/reboot.ts
+++ src/commands/host-trasport/reboot.ts
@@ -1,19 +1,11 @@
-import { Reply } from '../..';
 import TransportCommand from '../tranport';
 
-export default class RebootCommand extends TransportCommand {
+export default class RebootCommand extends TransportCommand<void> {
+    Cmd = 'reboot:';
+    protected postExecute(): Promise<void> {
+        return Promise.resolve();
+    }
     execute(serial: string): Promise<void> {
-        return super.execute(serial, 'reboot:').then((reply) => {
-            switch (reply) {
-                case Reply.OKAY:
-                    return this.parser.readAll().then(() => {});
-                case Reply.FAIL:
-                    return this.parser.readError().then((e) => {
-                        throw e;
-                    });
-                default:
-                    throw this.parser.unexpected(reply, 'OKAY or FAIL');
-            }
-        });
+        return this.preExecute(serial);
     }
 }
