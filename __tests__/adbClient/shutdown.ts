4220e0a1abceeeb149cf0413ecbe0be108c0855e
Martin Svoboda <svobma96@gmail.com>
2022-07-24 20:50:25 +0200

    Shutdown tests

---
+++ __tests__/adbClient/shutdown.ts
@@ -0,0 +1,113 @@
+import AdbMock from '../../mockery/mockAdbServer';
+import AdbClient from '../../lib/client';
+
+describe('Shutdown', () => {
+    it('OKAY', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'shell:reboot -p', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.shutdown('serial');
+            expect(result).toBe(void 0);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL first response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'fail', res: null, rawRes: true },
+            { cmd: 'shell:reboot -p', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.shutdown('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL second response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'fail', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.shutdown('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected first response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            },
+            { cmd: 'shell:reboot -p', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.shutdown('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected second response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: 'shell:reboot -p',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.shutdown('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
--- src/commands/host-trasport/shutdown.ts
+++ src/commands/host-trasport/shutdown.ts
@@ -1,19 +1,11 @@
-import { Reply } from '../..';
 import TransportCommand from '../tranport';
 
-export default class ShutdownCommand extends TransportCommand {
+export default class ShutdownCommand extends TransportCommand<void> {
+    Cmd = 'shell:reboot -p';
+    protected postExecute(): Promise<void> {
+        return Promise.resolve();
+    }
     execute(serial: string): Promise<void> {
-        return super.execute(serial, 'shell:reboot -p').then((reply) => {
-            switch (reply) {
-                case Reply.OKAY:
-                    return this.parser.readAll().then(() => {});
-                case Reply.FAIL:
-                    return this.parser.readError().then((e) => {
-                        throw e;
-                    });
-                default:
-                    throw this.parser.unexpected(reply, 'OKAY or FAIL');
-            }
-        });
+        return this.preExecute(serial);
     }
 }
