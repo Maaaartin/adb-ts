523dbc8581b7b66cb7f259b6e3ddbd7014367899
Martin Svoboda <svobma96@gmail.com>
2022-07-25 19:36:58 +0200

    Root tests

---
+++ __tests__/adbClient/root.ts
@@ -0,0 +1,127 @@
+import AdbMock from '../../mockery/mockAdbServer';
+import AdbClient from '../../lib/client';
+
+describe('Root', () => {
+    it('OKAY', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'root:', res: 'restarting adbd as root', rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.root('serial');
+            expect(result).toBe(void 0);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('OKAY invalid response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'root:', res: 'invalid', rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.root('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('invalid'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL first response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'fail', res: null, rawRes: true },
+            { cmd: 'root:', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.root('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL second response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'fail', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.root('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected first response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            },
+            { cmd: 'root:', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.root('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Unexpected second response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            { cmd: 'root:', res: null, rawRes: true, unexpected: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.root('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error("Unexpected 'ABCD', was expecting OKAY or FAIL")
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
--- src/commands/host-trasport/root.ts
+++ src/commands/host-trasport/root.ts
@@ -1,27 +1,13 @@
-import Command from '../../command';
-import { Reply } from '../..';
-import TransportCommand from '../tranport';
+import TransportParseAllCommand from '../transport-parse-all-command';
 
-export default class RootCommand extends TransportCommand {
+export default class RootCommand extends TransportParseAllCommand<void> {
+    protected parse(value: string): void {
+        if (!/restarting adbd as root/.test(value)) {
+            throw new Error(value);
+        }
+    }
+    Cmd = 'root:';
     execute(serial: string): Promise<void> {
-        return super.execute(serial, 'root:').then((reply) => {
-            switch (reply) {
-                case Reply.OKAY:
-                    return this.parser.readAll().then((value) => {
-                        const valueStr = value.toString();
-                        if (/restarting adbd as root/.test(valueStr)) {
-                            return;
-                        } else {
-                            throw new Error(valueStr.trim());
-                        }
-                    });
-                case Reply.FAIL:
-                    return this.parser.readError().then((e) => {
-                        throw e;
-                    });
-                default:
-                    throw this.parser.unexpected(reply, 'OKAY or FAIL');
-            }
-        });
+        return this.preExecute(serial);
     }
 }
