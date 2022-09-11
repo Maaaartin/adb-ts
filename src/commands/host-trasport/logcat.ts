55c7bf5b4933ff1009906ecd91ec9713e621e3db
Martin Svoboda <svobma96@gmail.com>
2022-09-11 18:38:21 +0200

    Refactor logcat command

--- src/commands/host-trasport/logcat.ts
+++ src/commands/host-trasport/logcat.ts
@@ -4,17 +4,17 @@ import RawCommand from '../raw-command';
 import { LogcatOptions } from '../..';
 
 export default class LogcatCommand extends RawCommand {
+    Cmd = 'shell:echo && ';
     execute(serial: string, options?: LogcatOptions): Promise<LineTransform> {
         let cmd = 'logcat -B *:I 2>/dev/null';
         if (options?.clear) {
             cmd = 'logcat -c 2>/dev/null && ' + cmd;
         }
-        return this.preExecute(serial, `shell:echo && ${cmd}`).then(
-            (result) => {
-                const transform = new LineTransform({ autoDetect: true });
-                result.pipe(transform);
-                return transform;
-            }
-        );
+        this.Cmd += cmd;
+        return this.preExecute(serial).then((result) => {
+            const transform = new LineTransform({ autoDetect: true });
+            result.pipe(transform);
+            return transform;
+        });
     }
 }
