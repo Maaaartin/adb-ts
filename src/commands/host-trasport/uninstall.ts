832c51748cf40c5a99211e56cc52bbeaf5f73eeb
Martin Svoboda <svobma96@gmail.com>
2022-10-18 20:40:13 +0200

    Fix errors in uninstall command

--- src/commands/host-trasport/uninstall.ts
+++ src/commands/host-trasport/uninstall.ts
@@ -1,35 +1,26 @@
-import { Reply, UninstallOptions } from '../..';
+import { UninstallOptions } from '../..';
 import TransportCommand from '../transport';
 
-export default class UninstallCommand extends TransportCommand {
-    execute(serial: string, pkg: string, options?: UninstallOptions) {
-        return super
-            .execute(
-                serial,
-                'shell:pm uninstall',
-                options?.keepCache ? '-k' : '',
-                pkg
-            )
-            .then((reply) => {
-                switch (reply) {
-                    case Reply.OKAY:
-                        return this.parser
-                            .searchLine(
-                                /^(Success|Failure.*|.*Unknown package:.*)$/
-                            )
-                            .then(() => {
-                                return;
-                            })
-                            .finally(() => {
-                                return this.parser.readAll();
-                            });
-                    case Reply.FAIL:
-                        return this.parser.readError().then((e) => {
-                            throw e;
-                        });
-                    default:
-                        throw this.parser.unexpected(reply, 'OKAY or FAIL');
-                }
+export default class UninstallCommand extends TransportCommand<void> {
+    Cmd = 'shell:pm uninstall';
+    protected postExecute(): Promise<void> {
+        return this.parser
+            .searchLine(/^(Success|Failure.*|.*Unknown package:.*)$/)
+            .then(() => {})
+            .finally(() => {
+                return this.parser.readAll();
             });
+    }
+    execute(
+        serial: string,
+        pkg: string,
+        options?: UninstallOptions
+    ): Promise<void> {
+        this.Cmd = [this.Cmd]
+            .concat(options?.keepCache ? '-k' : [])
+            .concat(pkg)
+            .join(' ');
+
+        return this.preExecute(serial);
     }
 }
