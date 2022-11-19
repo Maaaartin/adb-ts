3f9de51895c050852cd0a87f96939c78338799cd
Martin Svoboda <svobma96@gmail.com>
2022-11-19 20:39:43 +0100

    Tcpip tests

---
+++ __tests__/adbClient/tcpip.ts
@@ -0,0 +1,65 @@
+import AdbMock from '../../mockery/mockAdbServer';
+import AdbClient from '../../lib/client';
+import { FailError } from '../../lib';
+
+describe('Tcpip', () => {
+    it('OKAY with default port', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `tcpip:5555`,
+                res: 'restarting in',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.tcpip('serial');
+            expect(result).toBe(undefined);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('OKAY with passed port', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `tcpip:3333`,
+                res: 'restarting in',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.tcpip('serial', 3333);
+            expect(result).toBe(undefined);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL with non matching response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `tcpip:5555`,
+                res: 'error',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.tcpip('serial');
+            } catch (e) {
+                expect(e).toEqual(new FailError('error'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
