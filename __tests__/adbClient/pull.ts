c9ec7d5b9d11fbbbcc5f74b77a8e0a2d22e9e1b0
Martin Svoboda <svobma96@gmail.com>
2022-11-19 16:27:26 +0100

    Pull tests

--- __tests__/adbClient/pull.ts
+++ __tests__/adbClient/pull.ts
@@ -1,6 +1,7 @@
 import AdbMock from '../../mockery/mockAdbServer';
 import AdbClient from '../../lib/client';
 import { promisify } from 'util';
+import { FailError, UnexpectedDataError } from '../../lib';
 
 describe('Pull tests', () => {
     test('OKAY', async () => {
@@ -28,6 +29,76 @@ describe('Pull tests', () => {
                 });
             })();
             expect(result).toBe('data');
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    test('FAIL', async () => {
+        const buff = Buffer.from([4, 0, 0, 0]);
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'sync:',
+                res: 'FAIL' + buff.toString() + 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await promisify<string | null>(async (cb) => {
+                    const transfer = await adb.pull('serial', '/');
+                    let acc = '';
+                    transfer.on('error', (err) => cb(err, null));
+                    transfer.on('data', (data) => {
+                        acc += data.toString();
+                    });
+                    transfer.on('end', () => {
+                        cb(null, acc);
+                    });
+                })();
+                fail('Expected failure');
+            } catch (e) {
+                expect(e).toEqual(new FailError('data'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    test('Unexpected error', async () => {
+        const buff = Buffer.from([4, 0, 0, 0]);
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'sync:',
+                res: 'ABCD' + buff.toString() + 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await promisify<string | null>(async (cb) => {
+                    const transfer = await adb.pull('serial', '/');
+                    let acc = '';
+                    transfer.on('error', (err) => cb(err, null));
+                    transfer.on('data', (data) => {
+                        acc += data.toString();
+                    });
+                    transfer.on('end', () => {
+                        cb(null, acc);
+                    });
+                })();
+                fail('Expected failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('ABCD', 'DATA, DONE or FAIL')
+                );
+            }
         } finally {
             await adbMock.end();
         }
