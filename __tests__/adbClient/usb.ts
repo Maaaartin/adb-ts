09711eb764e5ffd918f3f6b1418efa8b47934cd2
Martin Svoboda <svobma96@gmail.com>
2022-11-20 14:15:25 +0100

    Usb tests

---
+++ __tests__/adbClient/usb.ts
@@ -0,0 +1,46 @@
+import AdbMock from '../../mockery/mockAdbServer';
+import AdbClient from '../../lib/client';
+import { FailError } from '../../lib';
+
+describe('Usb', () => {
+    it('OKAY with default port', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `usb:`,
+                res: 'restarting in',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.usb('serial');
+            expect(result).toBe(undefined);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('FAIL with non matching response', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `usb:`,
+                res: 'error',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.usb('serial');
+            } catch (e) {
+                expect(e).toEqual(new FailError('error'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
