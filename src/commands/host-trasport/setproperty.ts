320a340f8de3c1879b5e55cb75ce7622f3da65f1
Martin Svoboda <svobma96@gmail.com>
2022-11-25 22:58:27 +0100

    Ignore case at error parsing

--- src/commands/host-trasport/setproperty.ts
+++ src/commands/host-trasport/setproperty.ts
@@ -5,7 +5,7 @@ export default class SetProp extends TransportCommand<void> {
     protected postExecute(): Promise<void> {
         return this.parser.readAll().then((value) => {
             const valueStr = value.toString();
-            if (/failed/.test(valueStr)) {
+            if (/failed/i.test(valueStr)) {
                 throw new Error(valueStr);
             }
         });
