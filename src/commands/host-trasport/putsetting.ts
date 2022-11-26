afa301a9b1ef81a1e99141ed316293b2cb134fe1
Martin Svoboda <svobma96@gmail.com>
2022-11-26 22:34:52 +0100

    Fix put setting

--- src/commands/host-trasport/putsetting.ts
+++ src/commands/host-trasport/putsetting.ts
@@ -1,28 +1,23 @@
 import { SettingsMode, PrimitiveType } from '../..';
 import TransportCommand from '../transport';
 
-export default class PutSetting extends TransportCommand {
+export default class PutSetting extends TransportCommand<void> {
+    Cmd = 'shell:settings put ';
+    protected postExecute(): Promise<void> {
+        return this.parser.readAll().then((value) => {
+            const valueStr = value.toString();
+            if (/failed/.test(valueStr)) {
+                throw new Error(valueStr);
+            }
+        });
+    }
     execute(
         serial: string,
         mode: SettingsMode,
         name: string,
         value: PrimitiveType
-    ) {
-        return super
-            .execute(
-                serial,
-                'shell:settings put',
-                mode,
-                name,
-                this.escape(value)
-            )
-            .then(() => {
-                return this.parser.readAll().then((value) => {
-                    const valueStr = value.toString();
-                    if (/failed/.test(valueStr)) {
-                        throw new Error(valueStr);
-                    }
-                });
-            });
+    ): Promise<void> {
+        this.Cmd += [mode, name, this.escape(value)].join(' ');
+        return this.preExecute(serial);
     }
 }
