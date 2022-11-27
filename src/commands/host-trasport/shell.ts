eb3c0f17b4cef0e07a5ca59af7884cbf556cbf9e
Martin Svoboda <svobma96@gmail.com>
2022-11-27 19:50:36 +0100

    Refactor shell command

--- src/commands/host-trasport/shell.ts
+++ src/commands/host-trasport/shell.ts
@@ -1,14 +1,15 @@
-import { PrimitiveType, stringToType } from '../..';
-import TransportParseAllCommand from '../transport-parse-all-command';
+import TransportCommand from '../transport';
 
-export default class ShellCommand extends TransportParseAllCommand {
-    protected parse(value: string) {
-        return stringToType(value);
+export default class ShellCommand extends TransportCommand<string> {
+    Cmd = 'shell:';
+    protected postExecute(): Promise<string> {
+        return this.parser.readAll().then((value) => {
+            return value.toString();
+        });
     }
-    execute(
-        serial: string,
-        command: string | string[]
-    ): Promise<PrimitiveType> {
-        return super.execute(serial, 'shell:', command);
+
+    execute(serial: string, command: string | string[]): Promise<string> {
+        this.Cmd += [command].flat().join(' ');
+        return this.preExecute(serial);
     }
 }
