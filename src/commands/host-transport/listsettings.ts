83a0409a32fa36e36c198b29e690b289cb969052
Martin Svoboda <svobma96@gmail.com>
2022-11-27 16:32:34 +0100

    Fix list settings parsing, bump version

--- src/commands/host-transport/listsettings.ts
+++ src/commands/host-transport/listsettings.ts
@@ -7,7 +7,7 @@ export default class ListSettingsCommand extends TransportParseAllCommand {
   parse(value) {
     const settings: KeyStringObject = {};
     let match;
-    const regExp = /^([\s\S]*?)=([\s\S]*?)\n/gm;
+    const regExp = /^([\s\S]*?)=([\s\S]*?)$/gm;
     while ((match = regExp.exec(value))) {
       settings[match[1]] = stringToType(match[2] === '' ? undefined : match[2]);
     }
