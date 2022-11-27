2f32648343e31d0accd913ed5f146ddd4a80ba68
Martin Svoboda <svobma96@gmail.com>
2022-11-27 14:58:41 +0100

    Refactoring

--- src/commands/host-trasport/listproperties.ts
+++ src/commands/host-trasport/listproperties.ts
@@ -2,9 +2,8 @@ import { DataMap, findMatches } from '../..';
 import TransportParseAllCommand from '../transport-parse-all-command';
 
 export default class ListPropertiesCommand extends TransportParseAllCommand<DataMap> {
-    get Cmd(): string {
-        return 'shell:getprop';
-    }
+    Cmd = 'shell:getprop';
+
     protected parse(value: string): DataMap {
         return findMatches(value, /^\[([\s\S]*?)\]: \[([\s\S]*?)\]?$/gm, 'map');
     }
--- src/commands/host-trasport/listsettings.ts
+++ src/commands/host-trasport/listsettings.ts
@@ -3,9 +3,11 @@ import TransportParseAllCommand from '../transport-parse-all-command';
 
 export default class ListSettingsCommand extends TransportParseAllCommand<DataMap> {
     Cmd = 'shell:settings list ';
+
     parse(value: string): DataMap {
         return findMatches(value, /^([\s\S]*?)=([\s\S]*?)\n/gm, 'map');
     }
+
     execute(serial: string, mode: SettingsMode): Promise<DataMap> {
         this.Cmd += mode;
         return this.preExecute(serial);
