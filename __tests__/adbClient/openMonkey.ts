8314c92bd13368949cdafe0460f4e837e37dd3a6
Martin Svoboda <svobma96@gmail.com>
2022-12-03 17:14:14 +0100

    Unexpected reply tests for monkey

--- __tests__/adbClient/openMonkey.ts
+++ __tests__/adbClient/openMonkey.ts
@@ -147,4 +147,181 @@ describe('Open Monkey tests', () => {
             await adbMock.end();
         }
     });
+
+    test('Unexpected first response', async () => {
+        const adbMock = new AdbMockDouble([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:EXTERNAL_STORAGE=/data/local/tmp monkey --port ${1080} -v`,
+                res: ':Monkey:\n',
+                rawRes: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'tcp:1080', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+
+            try {
+                await adb.openMonkey('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    test('Unexpected second response', async () => {
+        const adbMock = new AdbMockDouble([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            },
+            {
+                cmd: `shell:EXTERNAL_STORAGE=/data/local/tmp monkey --port ${1080} -v`,
+                res: ':Monkey:\n',
+                rawRes: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'tcp:1080', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+
+            try {
+                await adb.openMonkey('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    test('Unexpected third response', async () => {
+        const adbMock = new AdbMockDouble([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:EXTERNAL_STORAGE=/data/local/tmp monkey --port ${1080} -v`,
+                res: ':Monkey:\n',
+                rawRes: true,
+                unexpected: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            { cmd: 'tcp:1080', res: null, rawRes: true }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+
+            try {
+                await adb.openMonkey('serial');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    // test('Unexpected fourth response', async () => {
+    //     const adbMock = new AdbMockDouble([
+    //         {
+    //             cmd: 'host:transport:serial',
+    //             res: null,
+    //             rawRes: true
+    //         },
+    //         { cmd: 'host:transport:serial', res: null, rawRes: true },
+    //         {
+    //             cmd: `shell:EXTERNAL_STORAGE=/data/local/tmp monkey --port ${1080} -v`,
+    //             res: ':Monkey:\n',
+    //             rawRes: true
+    //         },
+    //         ...Array(21).fill({
+    //             cmd: 'host:transport:serial',
+    //             res: null,
+    //             rawRes: true,
+    //             unexpected: true
+    //         }),
+    //         { cmd: 'tcp:1080', res: null, rawRes: true }
+    //     ]);
+    //     try {
+    //         const port = await adbMock.start();
+    //         const adb = new AdbClient({ noAutoStart: true, port });
+
+    //         try {
+    //             await adb.openMonkey('serial');
+    //             fail('Expected Failure');
+    //         } catch (e) {
+    //             expect(e).toEqual(
+    //                 new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+    //             );
+    //         }
+    //     } finally {
+    //         await adbMock.end();
+    //     }
+    // });
+
+    // test('Unexpected fifth response', async () => {
+    //     const adbMock = new AdbMockDouble([
+    //         {
+    //             cmd: 'host:transport:serial',
+    //             res: null,
+    //             rawRes: true
+    //         },
+    //         { cmd: 'host:transport:serial', res: null, rawRes: true },
+    //         {
+    //             cmd: `shell:EXTERNAL_STORAGE=/data/local/tmp monkey --port ${1080} -v`,
+    //             res: ':Monkey:\n',
+    //             rawRes: true
+    //         },
+    //         { cmd: 'host:transport:serial', res: null, rawRes: true },
+    //         { cmd: 'tcp:1080', res: null, rawRes: true, unexpected: true }
+    //     ]);
+    //     try {
+    //         const port = await adbMock.start();
+    //         const adb = new AdbClient({ noAutoStart: true, port });
+
+    //         try {
+    //             await adb.openMonkey('serial');
+    //             fail('Expected Failure');
+    //         } catch (e) {
+    //             expect(e).toEqual(
+    //                 new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+    //             );
+    //         }
+    //     } finally {
+    //         await adbMock.end();
+    //     }
+    // });
 });
