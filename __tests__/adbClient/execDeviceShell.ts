269392c40dedf2a105f55be4aa926e2350cfb2df
Martin Svoboda <svobma96@gmail.com>
2022-12-11 22:41:26 +0100

    Exec device tests

---
+++ __tests__/adbClient/execDeviceShell.ts
@@ -0,0 +1,44 @@
+import AdbClient from '../../lib/client';
+import { AdbExecError } from '../../lib';
+import { mockExec } from '../../mockery/execMock';
+
+describe('Exec device shell tests', () => {
+    it('Should execute without error', async () => {
+        mockExec(null, '', '');
+        const adb = new AdbClient({ noAutoStart: true });
+        const result = await adb.execDeviceShell('serial', 'cmd');
+        expect(result).toBe('');
+    });
+
+    it('Should execute with error', async () => {
+        mockExec(new Error('message'), '', '');
+        const adb = new AdbClient({ noAutoStart: true });
+        await expect(() =>
+            adb.execDeviceShell('serial', 'cmd')
+        ).rejects.toThrowError(new Error('message'));
+    });
+
+    it('Should execute with std error', async () => {
+        mockExec(null, '', 'message');
+        const adb = new AdbClient({ noAutoStart: true });
+        try {
+            await adb.execDeviceShell('serial', 'cmd');
+        } catch (e) {
+            expect(e.message).toBe('message');
+            expect(e.command).toBe('-s serial shell cmd');
+            expect(e).toBeInstanceOf(AdbExecError);
+        }
+    });
+
+    it('Should execute with std out matching error reg exp', async () => {
+        mockExec(null, 'Error: message', '');
+        const adb = new AdbClient({ noAutoStart: true });
+        try {
+            await adb.execDeviceShell('serial', 'cmd');
+        } catch (e) {
+            expect(e.message).toBe('Error: message');
+            expect(e.command).toBe('-s serial shell cmd');
+            expect(e).toBeInstanceOf(AdbExecError);
+        }
+    });
+});
