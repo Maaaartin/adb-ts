190660ddb2acbb09ffb17f32b46844f4ef62d72a
Martin Svoboda <svobma96@gmail.com>
2022-12-13 16:32:54 +0100

    Touch tests

---
+++ __tests__/adbClient/touch.ts
@@ -0,0 +1,252 @@
+import crypto from 'crypto';
+import { UnexpectedDataError } from '../../lib';
+import AdbClient from '../../lib/client';
+import { AdbMock } from '../../mockery/mockAdbServer';
+
+beforeAll(() => {
+    jest.spyOn(crypto, 'randomUUID').mockImplementation(() => {
+        return '123456';
+    });
+});
+const date = new Date('2022-12-13T12:41:42.418Z');
+describe('Touch OKAY tests', () => {
+    it('Should execute without parameters', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch /file) || echo '123456'`,
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.touch('serial', '/file');
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should execute with time as Date parameter', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch -a -t '202212131341.42418' /file) || echo '123456'`,
+                res: 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.touch('serial', '/file', {
+                aTime: true,
+                time: date
+            });
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should execute with time as string parameter', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch -a -t '202212131341.42418' /file) || echo '123456'`,
+                res: 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.touch('serial', '/file', {
+                aTime: true,
+                time: date.toISOString()
+            });
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should execute with date as Date parameter', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch -m -d '2022-12-13T12:41:42.418Z' /file) || echo '123456'`,
+                res: 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.touch('serial', '/file', {
+                mTime: true,
+                date: date
+            });
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should execute with date as string parameter', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch -m -d '2022-12-13T12:41:42.418Z' /file) || echo '123456'`,
+                res: 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.touch('serial', '/file', {
+                mTime: true,
+                date: date.toISOString()
+            });
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should execute with other parameter', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch -c -h -r '/file' /file) || echo '123456'`,
+                res: 'data',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const result = await adb.touch('serial', '/file', {
+                noCreate: true,
+                symlink: true,
+                reference: '/file'
+            });
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
+
+describe('Touch FAIL tests', () => {
+    it('First response should FAIL', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'fail', res: null, rawRes: true },
+            {
+                cmd: `shell:(touch /file) || echo '123456'`,
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.touch('serial', '/file');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Second response should FAIL', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `fail`,
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.touch('serial', '/file');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(new Error('Failure'));
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
+
+describe('Touch unexpected tests', () => {
+    it('Should throw unexpected error for first response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true,
+                unexpected: true
+            },
+            {
+                cmd: `shell:(touch /file) || echo '123456'`,
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.touch('serial', '/file');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should throw unexpected error for second response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: `shell:(touch /file) || echo '123456'`,
+                res: null,
+                rawRes: true,
+                unexpected: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            try {
+                await adb.touch('serial', '/file');
+                fail('Expected Failure');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('UNEX', 'OKAY or FAIL')
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
