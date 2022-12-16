5430bc7d86089b7aae655a120e30bdfc0fd8fbfd
Martin Svoboda <svobma96@gmail.com>
2022-12-16 21:14:53 +0100

    Fix File stat constructor

--- src/filestats.ts
+++ src/filestats.ts
@@ -31,25 +31,62 @@ export interface IFileStat {
 }
 
 export default class FileStat extends Stats implements IFileStat {
-    abits!: number;
-    aflags!: string;
-    allModeBits!: number;
-    atimems!: number;
-    bytes!: number;
-    ctimems!: number;
-    dTypeMajor!: number;
-    dTypeMinor!: number;
-    gname!: string;
-    moutpoint!: string;
-    mtimems!: number;
-    name!: string;
-    lname!: string;
-    seccon!: string;
-    type!: string;
-    uname!: string;
+    abits: number;
+    aflags: string;
+    atime: Date;
+    atimeMs: number;
+    blksize: number;
+    blocks: number;
+    bytes: number;
+    ctime: Date;
+    ctimeMs: number;
+    dev: number;
+    dTypeMajor: number;
+    dTypeMinor: number;
+    ino: number;
+    gid: number;
+    gname: string;
+    mode: number;
+    moutpoint: string;
+    mtime: Date;
+    mtimeMs: number;
+    name: string;
+    nlink: number;
+    lname: string;
+    seccon: string;
+    size: number;
+    type: string;
+    uid: number;
+    uname: string;
     constructor(props: IFileStat) {
         super();
-        Object.assign(this, props);
+        this.abits = props.abits;
+        this.aflags = props.aflags;
+        this.atime = props.atime;
+        this.atimeMs = props.atimeMs;
+        this.blksize = props.blksize;
+        this.blocks = props.blocks;
+        this.bytes = props.bytes;
+        this.ctime = props.ctime;
+        this.ctimeMs = props.ctimeMs;
+        this.dev = props.dev;
+        this.dTypeMajor = props.dTypeMajor;
+        this.dTypeMinor = props.dTypeMinor;
+        this.ino = props.ino;
+        this.gid = props.gid;
+        this.gname = props.gname;
+        this.mode = props.mode;
+        this.moutpoint = props.moutpoint;
+        this.mtime = props.mtime;
+        this.mtimeMs = props.mtimeMs;
+        this.name = props.name;
+        this.nlink = props.nlink;
+        this.lname = props.lname;
+        this.seccon = props.seccon;
+        this.size = props.size;
+        this.type = props.type;
+        this.uid = props.uid;
+        this.uname = props.uname;
     }
 
     isSocket(): boolean {
