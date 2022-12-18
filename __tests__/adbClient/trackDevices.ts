b6427dc2a6ad46dc472497f4f733b34f89dfe381
Martin Svoboda <svobma96@gmail.com>
2022-12-18 12:55:16 +0100

    Improve track device tests

--- __tests__/adbClient/trackDevices.ts
+++ __tests__/adbClient/trackDevices.ts
@@ -26,9 +26,20 @@ describe('Track devices', () => {
                 );
             })();
             try {
-                expect(result).toBeInstanceOf(AdbDevice);
+                expect(result).toEqual(
+                    new AdbDevice(adb, {
+                        id: 'b137f5dc',
+                        state: 'unauthorized',
+                        path: '337641472X',
+                        device: undefined,
+                        model: undefined,
+                        product: undefined,
+                        transportId: '1',
+                        transport: 'usb'
+                    })
+                );
             } finally {
-                await tracker.end();
+                tracker.end();
             }
         } finally {
             await adbMock.end();
@@ -56,46 +67,66 @@ describe('Track devices', () => {
                 adbMock.forceWrite('');
             })();
             try {
-                expect(result).toBeInstanceOf(Object);
+                expect(result).toEqual({
+                    id: 'b137f5dc',
+                    state: 'unauthorized',
+                    path: '337641472X',
+                    device: undefined,
+                    model: undefined,
+                    product: undefined,
+                    transportId: '1',
+                    transport: 'usb'
+                });
             } finally {
-                await tracker.end();
+                tracker.end();
             }
         } finally {
             await adbMock.end();
         }
     });
 
-    // it('Change', async () => {
-    //     const adbMock = new AdbMock({
-    //         cmd: 'host:track-devices-l',
-    //         res: 'b137f5dc               unauthorized usb:337641472X transport_id:1'
-    //     });
+    it('Change', async () => {
+        const adbMock = new AdbMock({
+            cmd: 'host:track-devices-l',
+            res: 'b137f5dc               unauthorized usb:337641472X transport_id:1'
+        });
 
-    //     try {
-    //         const port = await adbMock.start();
-    //         const adb = new AdbClient({ noAutoStart: true, port });
-    //         const tracker = await adb.trackDevices();
-    //         const result = await promisify((cb) => {
-    //             tracker.on('change', (d) => {
-    //                 cb(null, d);
-    //             });
-    //             tracker.on('error', (err) => {
-    //                 cb(err);
-    //             });
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const tracker = await adb.trackDevices();
+            const result = await promisify((cb) => {
+                tracker.on('change', (d) => {
+                    cb(null, d);
+                });
+                tracker.on('error', (err) => {
+                    cb(err, undefined);
+                });
 
-    //             adbMock.forceWriteData(
-    //                 'b137f5dc               device usb:337641472X transport_id:1'
-    //             );
-    //         })();
-    //         try {
-    //             expect(result).toBeInstanceOf(AdbDevice);
-    //         } finally {
-    //             await tracker.end();
-    //         }
-    //     } finally {
-    //         await adbMock.end();
-    //     }
-    // });
+                adbMock.forceWriteData(
+                    'b137f5dc               device usb:337641472X transport_id:1'
+                );
+            })();
+            try {
+                expect(result).toEqual(
+                    new AdbDevice(adb, {
+                        id: 'b137f5dc',
+                        state: 'device',
+                        path: '337641472X',
+                        device: undefined,
+                        model: undefined,
+                        product: undefined,
+                        transportId: '1',
+                        transport: 'usb'
+                    })
+                );
+            } finally {
+                tracker.end();
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
 
     it('Error', async () => {
         const adbMock = new AdbMock({
@@ -115,7 +146,7 @@ describe('Track devices', () => {
             try {
                 expect(result).toBeInstanceOf(Error);
             } finally {
-                await tracker.end();
+                tracker.end();
             }
         } finally {
             await adbMock.end();
@@ -141,7 +172,7 @@ describe('Track devices', () => {
             try {
                 expect(result).toBeUndefined();
             } finally {
-                await tracker.end();
+                tracker.end();
             }
         } finally {
             await adbMock.end();
@@ -165,12 +196,12 @@ describe('Track devices', () => {
                 tracker.on('end', () => {
                     cb(null, undefined);
                 });
-                await tracker.end();
+                tracker.end();
             })();
             try {
                 expect(result).toBeUndefined();
             } finally {
-                await tracker.end();
+                tracker.end();
             }
         } finally {
             await adbMock.end();
