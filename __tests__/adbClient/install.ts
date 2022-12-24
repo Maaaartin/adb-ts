b7eb5c26b66ce53da57c79ca6f38452a3993a555
Martin Svoboda <svobma96@gmail.com>
2022-12-24 19:31:29 +0100

    Add install test

--- __tests__/adbClient/install.ts
+++ __tests__/adbClient/install.ts
@@ -4,8 +4,8 @@ import { promisify } from 'util';
 import { FailError, UnexpectedDataError } from '../../src/util/errors';
 import { Readable } from 'stream';
 
-describe('Install tests', () => {
-    test('OKAY', async () => {
+describe('Install OKAY tests', () => {
+    it('Should install with Success response', async () => {
         const buff = Buffer.from([4, 0, 0, 0]);
         const adbMock = new AdbMockMulti([
             { cmd: 'host:transport:serial', res: null, rawRes: true },
@@ -39,6 +39,53 @@ describe('Install tests', () => {
                 Readable.from(Buffer.from([1, 0, 0, 0, 0, 0, 0]))
             );
             expect(result).toBeUndefined();
+        } finally {
+            adbMock.end();
+        }
+    });
+
+    it('Should install with Failure response', async () => {
+        const buff = Buffer.from([4, 0, 0, 0]);
+        const adbMock = new AdbMockMulti([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'sync:',
+                res: 'OKAY' + buff.toString(),
+                rawRes: true,
+                end: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:pm install "/data/local/tmp/_stream.apk"`,
+                res: 'Failure [6]\n',
+                rawRes: true,
+                end: true
+            },
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: `shell:'rm' '-f' '/data/local/tmp/_stream.apk'`,
+                res: '123',
+                rawRes: true,
+                end: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+
+            try {
+                await adb.install(
+                    'serial',
+                    Readable.from(Buffer.from([1, 0, 0, 0, 0, 0, 0]))
+                );
+                fail('Expected error');
+            } catch (e) {
+                expect(e).toEqual(
+                    new Error(
+                        '/data/local/tmp/_stream.apk could not be installed [6]'
+                    )
+                );
+            }
         } finally {
             adbMock.end();
         }
