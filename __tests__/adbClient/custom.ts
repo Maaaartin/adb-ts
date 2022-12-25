38b894d4e9ddc4b294490d67dd880dfaae0570f9
Martin Svoboda <svobma96@gmail.com>
2022-12-25 17:40:01 +0100

    Custom command test

---
+++ __tests__/adbClient/custom.ts
@@ -0,0 +1,29 @@
+import { AdbMock } from '../../mockery/mockAdbServer';
+import Command from '../../lib/command';
+import AdbClient from '../../lib/client';
+
+class TestCmd extends Command {
+    public execute(): Promise<null> {
+        return this.initExecute('').then(this.handleReply(null));
+    }
+}
+
+describe('Custom command tests', () => {
+    it('Should execute command', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: '',
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ port, noAutoStart: true });
+            const result = await adb.custom(TestCmd);
+            expect(result).toBeNull();
+        } finally {
+            adbMock.end();
+        }
+    });
+});
