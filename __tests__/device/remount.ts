1f84a7713d14ff498564084cf4c8496612b91598
Martin Svoboda <svobma96@gmail.com>
2022-12-26 22:11:23 +0100

    Device root and remount tests

--- __tests__/device/remount.ts
+++ __tests__/device/remount.ts
@@ -1,15 +1,15 @@
 import { AdbMock } from '../../mockery/mockAdbServer';
 import { getDevice } from '../../mockery/testDevice';
 
-describe('Shutdown tests', () => {
-    it('Should shutdown device', async () => {
+describe('Remount tests', () => {
+    it('Should remount device', async () => {
         const adbMock = new AdbMock([
             { cmd: 'host:transport:serial', res: null, rawRes: true },
-            { cmd: 'shell:reboot -p', res: null, rawRes: true }
+            { cmd: 'remount:', res: '', rawRes: true }
         ]);
         try {
             const port = await adbMock.start();
-            const result = await getDevice(port).shutdown();
+            const result = await getDevice(port).remount();
             expect(result).toBeUndefined();
         } finally {
             await adbMock.end();
