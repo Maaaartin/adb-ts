b883d34a5f8899d18366041ad40aed9d616def0e
Martin Svoboda <svobma96@gmail.com>
2022-12-26 21:49:50 +0100

    Device list forwards test

---
+++ __tests__/device/listForwards.ts
@@ -0,0 +1,33 @@
+import { AdbMock } from '../../mockery/mockAdbServer';
+import { getDevice } from '../../mockery/testDevice';
+
+describe('List forwards tests', () => {
+    it('Should list forwards', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host-serial:serial:list-forward',
+                res: `serial tcp:9222 localabstract:chrome_devtools_remote
+serial tcp:9223 localabstract:chrome_devtools_remote`
+            }
+        ]);
+
+        try {
+            const port = await adbMock.start();
+            const result = await getDevice(port).listForwards();
+            expect(result).toEqual([
+                {
+                    local: 'tcp:9222',
+                    remote: 'localabstract:chrome_devtools_remote',
+                    serial: 'serial'
+                },
+                {
+                    local: 'tcp:9223',
+                    remote: 'localabstract:chrome_devtools_remote',
+                    serial: 'serial'
+                }
+            ]);
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
