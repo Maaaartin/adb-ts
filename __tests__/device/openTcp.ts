a813b1b55a661c206e79b5d64610f4e327dc262f
Martin Svoboda <svobma96@gmail.com>
2022-12-26 22:56:20 +0100

    Device open tcp tests

---
+++ __tests__/device/openTcp.ts
@@ -0,0 +1,49 @@
+import Connection from '../../lib/connection';
+import { AdbMock } from '../../mockery/mockAdbServer';
+import { getDevice } from '../../mockery/testDevice';
+
+describe('Open tcp tests', () => {
+    it('Should open tcp without host', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: 'tcp:5555',
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const result = await getDevice(port).openTcp(5555);
+            expect(result).toBeInstanceOf(Connection);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should open tcp with host', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: 'tcp:localhost:5555',
+                res: null,
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const result = await getDevice(port).openTcp(5555, 'localhost');
+            expect(result).toBeInstanceOf(Connection);
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
