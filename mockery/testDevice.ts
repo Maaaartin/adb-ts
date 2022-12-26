6b77dfd5af322e782de6f08cbb519eb3ae5258d8
Martin Svoboda <svobma96@gmail.com>
2022-12-26 21:39:28 +0100

    Device get ip tests

---
+++ mockery/testDevice.ts
@@ -0,0 +1,15 @@
+import AdbClient from '../lib/client';
+import AdbDevice from '../lib/device';
+
+export const getDevice = (port: number): AdbDevice => {
+    return new AdbDevice(new AdbClient({ noAutoStart: true, port }), {
+        id: 'serial',
+        state: 'device',
+        path: 'path',
+        device: 'device',
+        model: 'model',
+        product: 'product',
+        transportId: 'transportId',
+        transport: 'usb'
+    });
+};
