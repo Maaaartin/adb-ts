38106b18e2cd1499bc32d83f694b3f6cb19001e8
Martin Svoboda <svobma96@gmail.com>
2022-12-26 18:19:35 +0100

    Device constructor test

---
+++ __tests__/device/constructor.ts
@@ -0,0 +1,26 @@
+import AdbClient from '../../lib/client';
+import AdbDevice from '../../lib/device';
+
+describe('Adb device constructor tests', () => {
+    it('should have the correct properties', () => {
+        const device = new AdbDevice(new AdbClient(), {
+            id: 'test',
+            state: 'device',
+            path: 'path',
+            device: 'device',
+            model: 'model',
+            product: 'product',
+            transportId: 'transportId',
+            transport: 'usb'
+        });
+
+        expect(device.id).toBe('test');
+        expect(device.state).toBe('device');
+        expect(device.path).toBe('path');
+        expect(device.device).toBe('device');
+        expect(device.model).toBe('model');
+        expect(device.product).toBe('product');
+        expect(device.transportId).toBe('transportId');
+        expect(device.transport).toBe('usb');
+    });
+});
