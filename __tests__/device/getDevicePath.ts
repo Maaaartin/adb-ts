a5a121042b10211de6674a5521902a4e1c8eb4e7
Martin Svoboda <svobma96@gmail.com>
2022-12-26 18:26:22 +0100

    Device get path test

---
+++ __tests__/device/getDevicePath.ts
@@ -0,0 +1,28 @@
+import AdbClient from '../../lib/client';
+import AdbDevice from '../../lib/device';
+import { AdbMock } from '../../mockery/mockAdbServer';
+
+describe('Get device path tests', () => {
+    it('Should get serial no', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host-serial:serial:get-devpath', res: 'usb:336592896X' }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const serialNo = await new AdbDevice(adb, {
+                id: 'serial',
+                state: 'device',
+                path: 'path',
+                device: 'device',
+                model: 'model',
+                product: 'product',
+                transportId: 'transportId',
+                transport: 'usb'
+            }).getDevicePath();
+            expect(serialNo).toBe('usb:336592896X');
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
