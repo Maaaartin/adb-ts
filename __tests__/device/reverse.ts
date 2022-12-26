665b963b42f7fd61d4e2d66489baa0bc8d748a77
Martin Svoboda <svobma96@gmail.com>
2022-12-26 21:54:57 +0100

    Device reverse test

---
+++ __tests__/device/reverse.ts
@@ -0,0 +1,25 @@
+import { AdbMock } from '../../mockery/mockAdbServer';
+import { getDevice } from '../../mockery/testDevice';
+
+describe('Reverse tests', () => {
+    it('Should reverse ports', async () => {
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'reverse:forward:localabstract:chrome_devtools_remote;tcp:9222',
+                res: 'OKAY',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const result = await getDevice(port).reverse(
+                'localabstract:chrome_devtools_remote',
+                'tcp:9222'
+            );
+            expect(result).toBeUndefined();
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
