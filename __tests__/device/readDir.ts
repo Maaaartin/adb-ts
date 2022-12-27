3b410c6132e9f1b28f24899dee2d3be936561fce
Martin Svoboda <svobma96@gmail.com>
2022-12-27 23:17:40 +0100

    Device read dir test

---
+++ __tests__/device/readDir.ts
@@ -0,0 +1,27 @@
+import AdbClient from '../../lib/client';
+import SyncEntry from '../../lib/sync/entry';
+import { AdbMock } from '../../mockery/mockAdbServer';
+
+describe('Device Read dir', () => {
+    it('Should read dir', async () => {
+        const buff = Buffer.from([
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 0, 0, 0
+        ]);
+        const adbMock = new AdbMock([
+            { cmd: 'host:transport:serial', res: null, rawRes: true },
+            {
+                cmd: 'sync:',
+                res: 'DENT' + buff.toString() + 'nameDONE' + buff.toString(),
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new AdbClient({ noAutoStart: true, port });
+            const [result] = await adb.readDir('serial', '/');
+            expect(result).toBeInstanceOf(SyncEntry);
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
