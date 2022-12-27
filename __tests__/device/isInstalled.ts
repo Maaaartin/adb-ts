5f43f3bae1bd38babfacaee2cdab5012f6fbc62e
Martin Svoboda <svobma96@gmail.com>
2022-12-27 14:40:06 +0100

    Device is installed tests

---
+++ __tests__/device/isInstalled.ts
@@ -0,0 +1,77 @@
+import { UnexpectedDataError } from '../../lib/util/errors';
+import { AdbMock } from '../../mockery/mockAdbServer';
+import { getDevice } from '../../mockery/testDevice';
+
+describe('Is installed tests', () => {
+    it('Should test that apk is installed', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: `shell:pm path com.package 2>/dev/null`,
+                res: 'package:\n',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const result = await getDevice(port).isInstalled('com.package');
+            expect(result).toBe(true);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should test that apk is not installed', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: `shell:pm path com.package 2>/dev/null`,
+                res: 'fail:\n',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            const result = await getDevice(port).isInstalled('com.package');
+            expect(result).toBe(false);
+        } finally {
+            await adbMock.end();
+        }
+    });
+
+    it('Should fail after getting wrong response', async () => {
+        const adbMock = new AdbMock([
+            {
+                cmd: 'host:transport:serial',
+                res: null,
+                rawRes: true
+            },
+            {
+                cmd: `shell:pm path com.package 2>/dev/null`,
+                res: 'badValue\n',
+                rawRes: true
+            }
+        ]);
+        try {
+            const port = await adbMock.start();
+            try {
+                await getDevice(port).isInstalled('com.package');
+                fail('Expected error');
+            } catch (e) {
+                expect(e).toEqual(
+                    new UnexpectedDataError('badValue', 'package:')
+                );
+            }
+        } finally {
+            await adbMock.end();
+        }
+    });
+});
