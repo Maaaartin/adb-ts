d3f2cd9dd3b9c4179049dc39edec4e24c9b3c5e3
Martin Svoboda <svobma96@gmail.com>
2023-01-12 22:22:24 +0100

    Refactor error constructors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,10 +1,8 @@
 export class FailError extends Error {
     constructor(message?: string) {
         super();
-        Object.setPrototypeOf(this, FailError.prototype);
         this.name = 'FailError';
         this.message = `Failure: ${message}`;
-        Error.captureStackTrace(this);
     }
 }
 
@@ -13,35 +11,29 @@ export class UnexpectedDataError extends Error {
     public expected: string;
     constructor(unexpected: string, expected: string) {
         super();
-        Object.setPrototypeOf(this, UnexpectedDataError.prototype);
         this.name = 'UnexpectedDataError';
         this.message = `Unexpected '${unexpected}', was expecting ${expected}`;
         this.unexpected = unexpected;
         this.expected = expected;
-        Error.captureStackTrace(this);
     }
 }
 export class PrematureEOFError extends Error {
     public missingBytes: number;
     constructor(howManyMissing: number) {
         super();
-        Object.setPrototypeOf(this, PrematureEOFError.prototype);
         this.name = 'PrematureEOFError';
         this.message =
             'Premature end of stream, needed ' + howManyMissing + ' more bytes';
         this.missingBytes = howManyMissing;
-        Error.captureStackTrace(this);
     }
 }
 
 export class NotConnectedError extends Error {
     constructor() {
         super();
-        Object.setPrototypeOf(this, NotConnectedError.prototype);
         this.name = 'NotConnectedError';
         this.message =
             'Client not connected. `connect` function must be called before use.';
-        Error.captureStackTrace(this);
     }
 }
 export class AdbError extends Error {
@@ -50,13 +42,11 @@ export class AdbError extends Error {
     path: string;
     constructor(message: string, errno: number, code: string, path: string) {
         super();
-        Object.setPrototypeOf(this, AdbError.prototype);
         this.name = 'AdbError';
         this.message = message;
         this.errno = errno;
         this.code = code;
         this.path = path;
-        Error.captureStackTrace(this);
     }
 }
 
@@ -64,10 +54,8 @@ export class AdbExecError extends Error {
     command: string;
     constructor(message: string, cmd: string) {
         super();
-        Object.setPrototypeOf(this, AdbExecError.prototype);
         this.name = 'AdbExecError';
         this.message = message;
         this.command = cmd;
-        Error.captureStackTrace(this);
     }
 }
