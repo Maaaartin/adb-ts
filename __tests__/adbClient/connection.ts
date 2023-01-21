abb53a1b5c9d18490390ab9ee8d45fc838aec9fc
Martin Svoboda <svobma96@gmail.com>
2023-01-21 18:34:17 +0100

    Fix connection test

--- __tests__/adbClient/connection.ts
+++ __tests__/adbClient/connection.ts
@@ -4,7 +4,7 @@ import { mockExec } from '../../mockery/execMock';
 describe('Client connection tests', () => {
     it('Should try to start server on ECONNREFUSED error', async () => {
         const mocked = mockExec(null);
-        const client = new AdbClient({ port: 0 });
+        const client = new AdbClient({ port: 1 });
         await expect(() => (client as any).connection()).rejects.toThrow();
         expect(mocked).toHaveBeenCalledTimes(1);
     });
