b1f3064fe89517c1616f1b99a7481c4a6dbe54a7
Martin Svoboda <svobma96@gmail.com>
2023-10-13 21:39:14 +0200

    Remove moment

--- src/commands/host-transport/fileSystem/touch.ts
+++ src/commands/host-transport/fileSystem/touch.ts
@@ -1,7 +1,21 @@
-import moment from 'moment';
 import { TouchOptions, escape } from '../../../util';
 import FileSystemCommand from '../../abstract/fileSystem';
 
+/**
+ * Formats to YYYYMMDDHHmm[.]ssSSS
+ */
+const formatToTimeFlag = (value: Date | string): string => {
+    const date = new Date(value);
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
+
+    return `${year}${month}${day}${hours}${minutes}.${seconds}${milliseconds}`;
+};
 export default class TouchCommand extends FileSystemCommand<
     TouchOptions,
     string
@@ -14,11 +28,11 @@ export default class TouchCommand extends FileSystemCommand<
         symlink: '-h',
         date: (value: Date | string): string[] => [
             '-d',
-            escape(moment(value).toISOString())
+            escape(new Date(value).toISOString())
         ],
         time: (value: Date | string): string[] => [
             '-t',
-            escape(moment(value).format('YYYYMMDDHHmm[.]ssSSS'))
+            escape(formatToTimeFlag(new Date(value)))
         ],
         reference: (value: string): string[] => ['-r', escape(value)]
     };
