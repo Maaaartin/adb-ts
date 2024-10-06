ec7741a3ee9d6d8dbf13f677903006434fdc2e06
Martin Svoboda <svobma96@gmail.com>
2024-10-06 19:31:11 +0200

    Refactor logcat test

--- __tests__/adbClient/openLogcatV2.ts
+++ __tests__/adbClient/openLogcatV2.ts
@@ -1,14 +1,34 @@
 import { Client } from '../../lib/client';
 import { AdbMock } from '../../mockery/mockAdbServer';
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
-import { LogcatReaderV2, PriorityV2 } from '../../lib/logcat';
+import { LogcatReaderV2 } from '../../lib/logcat';
+
+const readFile = (
+    dir: string,
+    file: 'raw.log' | 'output.json'
+): Promise<Buffer> =>
+    fs.readFile(path.join(__dirname, `../../mockery/data/${dir}/${file}`));
+
+type OutputArray = Record<string, unknown>[];
+
+const getLogcatTestData = (dir: string): Promise<[Buffer, OutputArray]> =>
+    Promise.all([
+        readFile(dir, 'raw.log'),
+        readFile(dir, 'output.json')
+            .then(String)
+            .then(JSON.parse)
+            .then((output_) =>
+                (output_ as OutputArray).map((entry) => ({
+                    ...entry,
+                    date: new Date(entry.date as string)
+                }))
+            )
+    ]);
 
 describe('Open logcat OKAY tests', () => {
-    const data = fs.readFileSync(
-        path.join(__dirname, '../../mockery/data/output.log')
-    );
-    it('Should read logs and safely exit', async () => {
+    it('Should read all logs when logs buffer ends with a new line', async () => {
+        const [data, output] = await getLogcatTestData('logsEndingWithNewLine');
         const adbMock = new AdbMock([
             { cmd: 'host:transport:serial', res: { raw: true } },
             {
@@ -17,20 +37,24 @@ describe('Open logcat OKAY tests', () => {
                 res: { value: data, raw: true }
             }
         ]);
+        try {
+            const port = await adbMock.start();
+            const adb = new Client({ noAutoStart: true, port });
+            const result = await adb.openLogcatV2('serial');
 
-        const port = await adbMock.start();
-        const adb = new Client({ noAutoStart: true, port });
-        const result = await adb.openLogcatV2('serial');
-
-        expect(result).toBeInstanceOf(LogcatReaderV2);
-        for await (const entry of result.logs()) {
-            expect(entry.date).toBeInstanceOf(Date);
-            expect(Number.isInteger(entry.pid)).toBe(true);
-            expect(Number.isInteger(entry.tid)).toBe(true);
-            expect(Object.values(PriorityV2)).toContain(entry.priority);
-            expect(typeof entry.tag).toBe('string');
-            expect(typeof entry.message).toBe('string');
+            expect(result).toBeInstanceOf(LogcatReaderV2);
+            const logsIterator = result.logs();
+            let count = 0;
+            for (const outputEntry of output) {
+                count++;
+                const entry = await logsIterator.next();
+                expect(entry.value).toEqual(outputEntry);
+            }
+            const { done } = await logsIterator.next();
+            expect(done).toBe(true);
+            expect(count).toBe(output.length);
+        } finally {
+            await adbMock.end();
         }
-        await adbMock.end();
     });
 });
