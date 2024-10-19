7536a68d321a138cd984870c02005a9d3790d641
Martin Svoboda <svobma96@gmail.com>
2024-10-19 12:26:57 +0200

    Test for grouped log chunks

--- __tests__/logcat/textParser.ts
+++ __tests__/logcat/textParser.ts
@@ -15,4 +15,26 @@ describe('TextParserGrouped tests', () => {
         const { done } = iterator.next();
         expect(done).toBe(true);
     });
+
+    it('should parse logs coming in chunks', async () => {
+        const dir = 'logsWithMultiLineErrors';
+        const parser = new TextParserGrouped();
+        const buffer = await readFile(dir, 'raw.log');
+        const halfLength = Math.floor(buffer.length / 2);
+        const firstChunk = buffer.subarray(0, halfLength);
+
+        const results = await readOutputFile(dir);
+        const resultsIterator = results.values();
+        for (const entry of parser.parse(firstChunk)) {
+            const { value } = resultsIterator.next();
+            expect(JSON.stringify(entry)).toEqual(value);
+        }
+        const secondChunk = buffer.subarray(halfLength);
+        for (const entry of parser.parse(secondChunk)) {
+            const { value } = resultsIterator.next();
+            expect(JSON.stringify(entry)).toEqual(value);
+        }
+        const { done } = resultsIterator.next();
+        expect(done).toBe(true);
+    });
 });
